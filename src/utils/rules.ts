34e173266d6723730c016643df8302fb1a0eb0ec
Duy Anh <ndanhdev@gmail.com>
2023-09-03 16:19:36 +0700

    Validate register form

---
+++ src/utils/rules.ts
@@ -0,0 +1,51 @@
+import type { RegisterOptions } from 'react-hook-form'
+
+type Rules = { [key in 'email' | 'password' | 'confirm_password']?: RegisterOptions }
+export const rules: Rules = {
+  email: {
+    required: {
+      value: true,
+      message: 'Email là bắt buộc'
+    },
+    pattern: {
+      value: /^\S+@\S+\.\S+$/,
+      message: 'Email không đúng định dạng'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 5 - 160 ký tự'
+    },
+    minLength: {
+      value: 5,
+      message: 'Độ dài từ 5 - 160 ký tự'
+    }
+  },
+  password: {
+    required: {
+      value: true,
+      message: 'Password là bắt buộc'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    },
+    minLength: {
+      value: 5,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    }
+  },
+  confirm_password: {
+    required: {
+      value: true,
+      message: 'Nhập lại password là bắt buộc'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    },
+    minLength: {
+      value: 5,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    }
+  }
+}
