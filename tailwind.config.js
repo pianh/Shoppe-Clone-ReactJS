a911aeda28ea7c75209f2eefd9d8588d0b23013d
Duy Anh <ndanhdev@gmail.com>
2023-09-03 14:49:25 +0700

    UI Login & Register

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // eslint-disable-next-line prettier/prettier
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: '#ee4d2d'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
