4fe5b973da12aba41aab8500789c5f9db40a2a30
Duy Anh <ndanhdev@gmail.com>
2023-09-03 21:15:14 +0700

    Tao component Input

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,26 @@
+import type { RegisterOptions, UseFormRegister } from 'react-hook-form'
+
+interface Props {
+  type: React.InputHTMLAttributes<HTMLInputElement>['type']
+  errorMessage?: string
+  placeholder?: string
+  className?: string
+  name: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  register: UseFormRegister<any>
+  rules?: RegisterOptions
+}
+
+export default function Input({ type, errorMessage, placeholder, className, name, register, rules }: Props) {
+  return (
+    <div className={className}>
+      <input
+        type={type}
+        className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+        placeholder={placeholder}
+        {...register(name, rules)}
+      />
+      <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>{errorMessage}</div>
+    </div>
+  )
+}
