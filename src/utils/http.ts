cd7dc5c1088d201435ecaa82c39bb888be581eda
Duy Anh <ndanhdev@gmail.com>
2023-09-04 16:06:59 +0700

    4:15

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -10,6 +10,19 @@ class Http {
         'Content-Type': 'application/json'
       }
     })
+    this.instance.interceptors.response.use(
+      function (response) {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        return response
+      },
+      function (error) {
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Do something with response error
+        console.log(error)
+        return Promise.reject(error)
+      }
+    )
   }
 }
 
